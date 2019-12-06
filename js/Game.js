77476ccafa56a9c4b61d38a9dfcd7f40373d9b87
FilipCiesielski <filip.carpenter@gmail.com>
2019-12-06 21:17:23 +0100

    the game logic works

--- js/Game.js
+++ js/Game.js
@@ -4,12 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const gridContainer = document.querySelector("#grid__container");
 
     const colors = [
-        orange = "rgb(255,186,28)",
-        blue = "rgb(101,124,255)",
-        lightblue = "rgb(79,219,255)",
-        violet = "rgb(125,98,163)",
-        red = "rgb(255,140,80)"
-
+        "rgb(255,186,28)",
+        "rgb(101,124,255)",
+        "rgb(79,219,255)",
+        "rgb(125,98,163)",
+        "rgb(255,140,80)"
     ];
 
     //  let button=document.querySelector("button");
@@ -19,24 +18,24 @@ document.addEventListener("DOMContentLoaded", () => {
     //
     //  let createRows=document.getElementById("createRows").value;
     // let createColumns = document.getElementById("createColumns").value;
-    //  if(createColumns==="" && createRows==="") {
-    //      alert( "please enter the number of columns and rows")
-    //
-    //  }else if(createRows==="" ) {
-    //
-    //      alert("please enter the number of rows")
-    //  }else if (createColumns==="" ){
-    //
-    //    alert ("please enter the number of columns")
-    //  }
-    //
-    //      let scoreBox=document.querySelector("span");
-    //      if(createRows!=="" && createColumns!=="")
-    //          scoreBox.className="";
-    //
-    //      makeGrid(createRows, createColumns);
-    //
+    //  // if(createColumns==="" && createRows==="") {
+    //  //     alert( "please enter the number of columns and rows")
+    //  //
+    //  // }else if(createRows==="" ) {
+    //  //
+    //  //     alert("please enter the number of rows")
+    //  // }else if (createColumns==="" ){
+    //  //
+    //  //   alert ("please enter the number of columns")
+    //  // }
+    //  //
+    //  //     let scoreBox=document.querySelector("span");
+    //  //     if(createRows!=="" && createColumns!=="")
+    //  //         scoreBox.className="";
+    //      makeGrid(createRows,createColumns)
     //  })
+
+
     makeGrid(8, 12);
 
     function makeGrid(rows, cols) {
@@ -45,36 +44,33 @@ document.addEventListener("DOMContentLoaded", () => {
         for (el = 0; el < (rows * cols); el++) {
             let cell = document.createElement("div");
             cell.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-
-
             const rowPositionClass = Math.floor([el] / cols);
             const colPositionClass = [el] % cols;
             gridContainer.appendChild(cell).className = `${rowPositionClass + 1}/${colPositionClass + 1}`;
         }
-
-        var score=0
+        var score = 0;
         let gridElements = document.getElementById("grid__container").children;
         Array.from(gridElements).map((el, index) =>
             el.addEventListener("click", () => {
                     let similarDivsIndex = [index];
-                let scoreCounter=document.querySelector(".box__score--result");
+                    let scoreCounter = document.querySelector(".box__score--result");
+                    console.log(scoreCounter)
                     checkNeighbourDiv(index, el.style.backgroundColor, gridElements, cols, similarDivsIndex);
+
                     if (similarDivsIndex.length > 1) {
-                       score+=similarDivsIndex.length;
-                        console.log(score);
-                        scoreCounter.textContent=score;
+
+                        score += similarDivsIndex.length;
+                        scoreCounter.textContent = score;
                         el.style.backgroundColor = "white";
                         fillSimilarDivs(similarDivsIndex, cols, gridElements);
                     }
 
                 }
-
             ));
-    }
+    };
 
 
-
-    function checkDiv(index, colorToCheck, gridElements, cols, similarDivsIndex) {
+ const checkDiv=(index, colorToCheck, gridElements, cols, similarDivsIndex) => {
         var currentDiv = gridElements[index];
         var color = currentDiv.style.backgroundColor;
 
@@ -86,9 +82,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         }
 
-    }
+    };
 
-    function checkNeighbourDiv(index, colorToCheck, gridElements, cols, similarDivsIndex) {
+    const checkNeighbourDiv = (index, colorToCheck, gridElements, cols, similarDivsIndex) => {
         let currentRow = Math.floor(index / cols);
         let firstIndexInCurrentRow = currentRow * cols;
         let lastIndexInCurrentRow = firstIndexInCurrentRow + cols - 1;
@@ -112,30 +108,55 @@ document.addEventListener("DOMContentLoaded", () => {
         if (bottomIndex < gridElements.length && !similarDivsIndex.includes(bottomIndex)) {
             checkDiv(bottomIndex, colorToCheck, gridElements, cols, similarDivsIndex);
         }
-    }
+    };
 
-    function fillSimilarDivs(similarDivIndex, cols, gridElements) {
-        console.log(cols);
-        similarDivIndex.sort((a, b) => b - a).forEach((index) => fillEmptyDiv(index, similarDivIndex, cols, gridElements));
+    const fillSimilarDivs = (similarDivIndex, cols, gridElements) => {
+        similarDivIndex.sort((a, b) => b - a).forEach((index) => fillEmptyDivsInColumn(index, gridElements, cols))
 
-    }
+    };
 
-    function fillEmptyDiv(index, similarDivIndex, cols, gridElements) {
 
-        let columnIndex = index % cols;
-        console.log(columnIndex);
-        console.log(index,columnIndex,cols);
-        var i=index;
-        for (;i > columnIndex; i-=cols) {
-            let divTop = gridElements[index - cols];
+    const NOT_FOUND_INDEX = -1;
+    const fillEmptyDivsInColumn = (emptyDivIndex, gridElements, cols) => {
+        var lastEmptyIndex = emptyDivIndex;
+        let columnIndex = getColumnIndex(lastEmptyIndex, cols);
 
-            if (divTop.style.backgroundColor !== "white") {
-                let divTopColor = divTop.style.backgroundColor;
-                gridElements[index].style.backgroundColor = divTopColor;
-                divTop.style.backgroundColor = "white";
+        while (lastEmptyIndex >= columnIndex) {
+            let emptyDiv = gridElements[lastEmptyIndex];
+            let firstNotEmptyDivIndex = getFirstNotEmptyDivIndex(lastEmptyIndex - cols, columnIndex, gridElements, cols);
+
+            if (firstNotEmptyDivIndex == NOT_FOUND_INDEX) {
+                emptyDiv.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+            } else {
+                let notEmptyDiv = gridElements[firstNotEmptyDivIndex];
+                emptyDiv.style.backgroundColor = notEmptyDiv.style.backgroundColor;
+                notEmptyDiv.style.backgroundColor = "white";
             }
 
+            lastEmptyIndex = getLastEmptyDivIndex(lastEmptyIndex - cols, columnIndex, gridElements, cols);
         }
+    };
 
+    const getLastEmptyDivIndex = (startIndex, columnIndex, gridElements, cols) => {
+        for (let i = startIndex; i >= columnIndex; i -= cols) {
+            if (gridElements[i].style.backgroundColor == "white") {
+                return i;
+            }
+        }
+        return NOT_FOUND_INDEX;
+    };
+
+    const getFirstNotEmptyDivIndex = (startIndex, columnIndex, gridElements, cols) => {
+        for (let i = startIndex; i >= columnIndex; i -= cols) {
+            if (gridElements[i].style.backgroundColor != "white") {
+                return i;
+            }
+        }
+        return NOT_FOUND_INDEX;
+    };
+
+    const getColumnIndex = (index, cols) => {
+        return index % cols;
     }
+
 });
